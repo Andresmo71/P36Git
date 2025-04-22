9ccfd3001d82c7ae9a229298e86c55ac44e41b6d
Andresmo71 <jose.maranon@educa.madrid.org>
2025-04-22 19:11:56 +0200

    Tercer commit:nuevo cambio tras revertir , el segundo syso  es deiferente

--- src/pack/P36.java
+++ src/pack/P36.java
@@ -3,5 +3,6 @@ package pack;
 public class P36 {
     public static void main(String[] args) {
         System.out.println("Primer commit");
+        System.out.println("Tercer commit tras revertir el anterior");
     }
 }
