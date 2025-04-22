627e0fb786ac404b7a771b685a563550f80a5abd
Andresmo71 <jose.maranon@educa.madrid.org>
2025-04-22 19:07:23 +0200

    Segundo commit: ahora hay 2 sysos

--- src/pack/P36.java
+++ src/pack/P36.java
@@ -3,5 +3,7 @@ package pack;
 public class P36 {
     public static void main(String[] args) {
         System.out.println("Primer commit");
+        System.out.println("Segundo commit que luego sera revertido");
+        
     }
 }
